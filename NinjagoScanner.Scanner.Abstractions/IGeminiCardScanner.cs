f7a1dad17343b3ade8fd767fce3a486795f55f71
Sebastian Martens <sebastian.martens@hotmail.de>
2026-08-05 22:47:33 +0200

    implement Catalog and Gemini API services; add series catalog loading and card analysis functionality

--- NinjagoScanner.Scanner.Abstractions/IGeminiCardScanner.cs
+++
@@ -1,6 +0,0 @@
-namespace NinjagoScanner.Scanner.Abstractions;
-
-public interface IGeminiCardScanner
-{
-    Task<GeminiScanSummary> ScanAsync(GeminiScanRequest? request = null, CancellationToken cancellationToken = default);
-}
